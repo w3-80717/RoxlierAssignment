80f474bff345cfac3760e0fcae61f5a9b84f91a6
w3-80717 <chavan1609@gmail.com>
2024-10-21 07:11:09 +0530

    return the mognoose connection in the database.js connect function

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 exports.connect = () => {
-    mongoose.connect(process.env.MONGO_URI, {
+    return mongoose.connect(process.env.MONGO_URI, {
         user: process.env.MONGO_USER,
         pass: process.env.MONGO_PASS,
         dbName: "RoxlierAssignment"
